a26d134ec84acacb8e486aa0956b331867a66018
Sean Murphy <filmmakersean@fastmail.net>
2019-08-04 02:29:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,7 +16,7 @@ class Song
   def self.new_by_filename(filename)
     song_array = filename.split(" - ")
     song = self.new(song_array[0])
-    song.artist = Artist.find_or_create_by_name(song_array[1])
+    song.artist(Artist.find_or_create_by_name(song_array[1]))
   end
   
 end
